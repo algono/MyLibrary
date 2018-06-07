cffeea330616a1fba85ee34cef8b203f7ddec8f2
Alejandro Gómez <algono@inf.upv.es>
2018-06-07 16:40:41 +0200

    Fixing some errors the last implementation had.

--- src/myLibrary/javafx/LoadingUtils/LoadingScene.java
+++ src/myLibrary/javafx/LoadingUtils/LoadingScene.java
@@ -23,12 +23,15 @@ public class LoadingScene extends Scene {
     
     //Default width and height values
     public static final double DEF_WIDTH = 300, DEF_HEIGHT = 175;
+    
     //Stage elements
     protected final Label label = new Label();
     protected final ProgressIndicator progress = new ProgressIndicator();
+    
     //The main task runs the tasks from the queue secuentially
     protected final LoadingQueue loadingQueue;
     protected final ChangeListener<Boolean> startListener;
+    
     //Constructors
     public LoadingScene(Task... tasks) {
         this(DEF_WIDTH, DEF_HEIGHT, tasks);
@@ -37,24 +40,32 @@ public class LoadingScene extends Scene {
         super(new VBox(10), width, height);
         VBox root = (VBox) getRoot();
         loadingQueue = new LoadingQueue(tasks);
+        
         //Sets the default root alignment and elements
         root.setAlignment(Pos.CENTER);
         root.getChildren().addAll(label, progress);
+        
         //Sets the text color bluish by default
         label.setTextFill(Color.web("#3d81e3"));
+        
         //Binds the label's text and the indicator's progress to the main task's message and progress
         label.textProperty().bind(loadingQueue.messageProperty());
-        progress.progressProperty().bind(loadingQueue.progressProperty());
+        if (!loadingQueue.isProgressIndeterminate()) progress.progressProperty().bind(loadingQueue.progressProperty());
+        
         //Listener that starts the loading process
         startListener = (obs, oldValue, newValue) -> {
             if (newValue) { loadingQueue.start(); }
         };
+        
         //Add the properties to any new scene's window so that the tasks start when the scene is shown
         windowProperty().addListener((obsWin, oldWindow, newWindow) -> {
+            
             //Remove the listener from the previous window (if there's any)
             if (oldWindow != null) oldWindow.showingProperty().removeListener(startListener);
+            
             //If the window is already showing, start the process now
             if (newWindow.isShowing()) loadingQueue.start();
+            
             newWindow.showingProperty().addListener(startListener);
         });
     }

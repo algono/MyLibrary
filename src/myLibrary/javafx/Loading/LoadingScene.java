4500d6818eb9fff9f72dc50d26549e14e52b23ca
Alejandro <Alejandro@192.168.1.140>
2018-06-15 19:36:28 +0200

    Now LoadingScene can use a ProgressBar instead of a ProgressIndicator. Through setProgressBar(boolean) you can choose between using:  - ProgressBar (true)  - ProgressIndicator (false).

--- src/myLibrary/javafx/Loading/LoadingScene.java
+++ src/myLibrary/javafx/Loading/LoadingScene.java
@@ -10,6 +10,7 @@ import javafx.concurrent.Worker;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -20,15 +21,22 @@ import javafx.scene.paint.Color;
  */
 public class LoadingScene extends Scene {
     
-    //Default width and height values
+    //Default width and height values (300 x 175)
     public static final double DEF_WIDTH = 300, DEF_HEIGHT = 175;
     
     //Stage elements
-    protected final Label label = new Label("Loading...");
-    protected final ProgressIndicator progress = new ProgressIndicator();
+    private final VBox root;
+    private Label label = new Label("Loading...");
+    private final ProgressIndicator progressIndicator = new ProgressIndicator();
+    private final ProgressBar progressBar = new ProgressBar();
+    
+    //If true, the progressBar is shown instead of the progressIndicator (and the other way around)
+    private boolean isProgressBar = false;
     
     //The main Service runs the workers from the queue secuentially
     protected final LoadingQueue loadingQueue;
+    
+    //Listener for starting the loading Service when the scene is shown
     protected final ChangeListener<Boolean> startListener;
     
     //Constructors
@@ -37,19 +45,20 @@ public class LoadingScene extends Scene {
     }
     public LoadingScene(double width, double height, Worker... workers) {
         super(new VBox(10), width, height);
-        VBox root = (VBox) getRoot();
+        root = (VBox) getRoot();
         loadingQueue = new LoadingQueue(workers);
         
         //Sets the default root alignment and elements
         root.setAlignment(Pos.CENTER);
-        root.getChildren().addAll(label, progress);
+        root.getChildren().addAll(label, progressIndicator);
         
         //Sets the text color bluish by default
         label.setTextFill(Color.web("#3d81e3"));
         
         //Listeners/bindings to the message and progress properties
         loadingQueue.messageProperty().addListener((obs, oldMsg, newMsg) -> label.setText(newMsg));
-        progress.progressProperty().bind(loadingQueue.progressProperty());
+        progressIndicator.progressProperty().bind(loadingQueue.progressProperty());
+        progressBar.progressProperty().bind(loadingQueue.progressProperty());
         
         //Listener that starts the loading process
         startListener = (obs, oldValue, newValue) -> {
@@ -71,8 +80,25 @@ public class LoadingScene extends Scene {
     
     //Getters
     public Label getLabel() { return label; }
-    public ProgressIndicator getProgressIndicator() { return progress; }
+    public ProgressIndicator getProgressIndicator() { return isProgressBar ? progressBar : progressIndicator; }
     public LoadingQueue getQueue() { return loadingQueue; }
+    
+    //Setters
+    public void setLabel(Label l) { label = l; }
+    public void setProgressBar(boolean willBeProgressBar) {
+        if (isProgressBar != willBeProgressBar) { //If the value is actually changing, do the changing based on the new value
+            if (willBeProgressBar) {
+                root.getChildren().remove(progressIndicator);
+                root.getChildren().add(progressBar);
+            } else {
+                root.getChildren().remove(progressBar);
+                root.getChildren().add(progressIndicator);
+            }
+            isProgressBar = willBeProgressBar;
+        } 
+    }
+    //Sets the root back to the original root made by the constructor
+    public void resetRoot() { setRoot(root); }
     
     //Getting if workers succeeded or not
     public boolean waitFor() { return loadingQueue.waitFor(); }

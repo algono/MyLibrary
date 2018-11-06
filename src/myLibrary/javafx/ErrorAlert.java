d0ab75448e55268979726e885728ad314e3608d4
Alejandro <Alejandro@192.168.1.140>
2018-11-06 21:31:55 +0100

    Added ErrorAlert.java (Alert version showing Exception traces).

---
+++ src/myLibrary/javafx/ErrorAlert.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myLibrary.javafx;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class ErrorAlert extends Alert {
+    
+    public ErrorAlert(Throwable ex) {
+        super(Alert.AlertType.ERROR);
+        init(ex);
+    }
+    
+    public ErrorAlert(Throwable ex, String contentText, ButtonType... buttons) {
+        super(Alert.AlertType.ERROR, contentText, buttons);
+        init(ex);
+    }
+    
+    private void init(Throwable ex) {
+        setHeaderText(null);
+        String errorOutput = (ex == null) ? null : ex.getMessage();
+        if (errorOutput != null && !errorOutput.isEmpty()) {
+            TextArea errorContent = new TextArea(errorOutput);
+            errorContent.setEditable(false); errorContent.setWrapText(true);
+            getDialogPane().setExpandableContent(new VBox(errorContent));
+        }
+    }
+    
+}
